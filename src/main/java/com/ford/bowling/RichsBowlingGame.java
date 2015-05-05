d8f9909b6edaa97266417b0daccede487288f373
Rich Dammkoehler <rpd@noradltd.com>
2015-05-05 17:25:10 -0400

    handled simple strike part of the way, much code rearranging

--- src/main/java/com/ford/bowling/RichsBowlingGame.java
+++ src/main/java/com/ford/bowling/RichsBowlingGame.java
@@ -2,18 +2,26 @@ package com.ford.bowling;
 
 public class RichsBowlingGame implements BowlingGame {
 
-  private int score = 0;
+  private int[] rolls = new int[21];
 
   @Override
   public Integer getTotalScore() {
+    int score = 0;
+    for (int frame = 1; frame < 11; frame++) {
+      int frameOffset = (frame-1)*2;
+      if ( rolls[frameOffset] == 10 ) {
+        //strike!
+        int bonus = rolls[frameOffset+2] + rolls[frameOffset+3];
+        score += bonus;
+      }
+      score += rolls[frameOffset] + rolls[frameOffset+1];
+    }
     return score;
   }
 
   @Override
   public void createFrame(int frame, int... rolls) {
-    for (int roll : rolls) {
-      score += roll;
-    }
+    System.arraycopy(rolls, 0, this.rolls, (frame-1)*2, rolls.length);
   }
 
 }

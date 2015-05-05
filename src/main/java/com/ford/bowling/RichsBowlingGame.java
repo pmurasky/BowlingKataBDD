6793c2548c783d7f7150081e48e9276a68ec2dfb
Rich Dammkoehler <rpd@noradltd.com>
2015-05-05 17:27:10 -0400

    refactor, method extracts to make a bit more readable

--- src/main/java/com/ford/bowling/RichsBowlingGame.java
+++ src/main/java/com/ford/bowling/RichsBowlingGame.java
@@ -8,8 +8,8 @@ public class RichsBowlingGame implements BowlingGame {
   public Integer getTotalScore() {
     int score = 0;
     for (int frame = 1; frame < 11; frame++) {
-      int frameOffset = (frame-1)*2;
-      if ( rolls[frameOffset] == 10 ) {
+      int frameOffset = frameOffset(frame);
+      if ( isStrike(frameOffset) ) {
         //strike!
         int bonus = rolls[frameOffset+2] + rolls[frameOffset+3];
         score += bonus;
@@ -21,7 +21,15 @@ public class RichsBowlingGame implements BowlingGame {
 
   @Override
   public void createFrame(int frame, int... rolls) {
-    System.arraycopy(rolls, 0, this.rolls, (frame-1)*2, rolls.length);
+    System.arraycopy(rolls, 0, this.rolls, frameOffset(frame), rolls.length);
+  }
+  
+  private boolean isStrike(int frameOffset) {
+    return rolls[frameOffset] == 10;
+  }
+
+  private int frameOffset(int frame) {
+    return (frame-1)*2;
   }
 
 }

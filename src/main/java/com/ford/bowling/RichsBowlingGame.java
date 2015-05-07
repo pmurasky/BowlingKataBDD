eac8a0a15e938669f73847abee3f21283730f089
Rich Dammkoehler <rpd@noradltd.com>
2015-05-07 07:52:20 -0400

    extracted Frame from Game

--- src/main/java/com/ford/bowling/RichsBowlingGame.java
+++ src/main/java/com/ford/bowling/RichsBowlingGame.java
@@ -11,86 +11,6 @@ public class RichsBowlingGame implements BowlingGame {
 
   @Override
   public Integer getTotalScore() {
-    class Frame {
-      int[] frameRolls = new int[5];
-      boolean tenthFrame = false, ninthFrame = false;
-
-      Frame(int frameNumber, int rollOffset, int[] rolls) {
-        ninthFrame = rollOffset == 16;
-        tenthFrame = rollOffset == 18;
-        System.arraycopy(rolls, rollOffset, frameRolls, 0, Math.min(5, 21 - rollOffset));
-      }
-
-      int getScore() {
-        int score = baseScore();
-        if (isStrike()) {
-          score += strikeBonus();
-        }
-        if (isSpare()) {
-          score += spareBonus();
-        }
-        return score;
-      }
-
-      private boolean isStrike() {
-        return firstBall() == 10 && (tenthFrame || secondBall() == 0);
-      }
-
-      private boolean nextFrameIsStrike() {
-        return firstBallOfNextFrame() == 10;
-      }
-
-      private boolean isSpare() {
-        return baseScore() == 10 && secondBall() > 0;
-      }
-
-      private int baseScore() {
-        return firstBall() + secondBall();
-      }
-
-      private int strikeBonus() {
-        int bonus = 0;
-        if (tenthFrame) {
-          bonus = bonusBall();
-        } else if (ninthFrame) {
-          bonus = firstBallOfNextFrame() + secondBallOfNextFrame();
-        } else if (nextFrameIsStrike()) {
-          bonus = firstBallOfNextFrame() + firstBallOfNextFramesNextFrame();
-        } else {
-          bonus = firstBallOfNextFrame() + secondBallOfNextFrame();
-        }
-        return bonus;
-      }
-
-      private int spareBonus() {
-        return firstBallOfNextFrame();
-      }
-
-      private int firstBall() {
-        return frameRolls[0];
-      }
-
-      private int secondBall() {
-        return frameRolls[1];
-      }
-
-      private int firstBallOfNextFrame() {
-        return frameRolls[2];
-      }
-
-      private int secondBallOfNextFrame() {
-        return frameRolls[3];
-      }
-
-      private int firstBallOfNextFramesNextFrame() {
-        return frameRolls[4];
-      }
-
-      private int bonusBall() {
-        return firstBallOfNextFrame();
-      }
-
-    }
     int altScore = 0;
     for (int frame = 1; frame < 11; frame++) {
       int rollOffset = rollOffsetFor(frame);
@@ -103,4 +23,85 @@ public class RichsBowlingGame implements BowlingGame {
     return (frame - 1) * 2;
   }
 
+}
+
+class Frame {
+  int[] frameRolls = new int[5];
+  boolean tenthFrame = false, ninthFrame = false;
+  
+  Frame(int frameNumber, int rollOffset, int[] rolls) {
+    ninthFrame = rollOffset == 16;
+    tenthFrame = rollOffset == 18;
+    System.arraycopy(rolls, rollOffset, frameRolls, 0, Math.min(5, 21 - rollOffset));
+  }
+  
+  int getScore() {
+    int score = baseScore();
+    if (isStrike()) {
+      score += strikeBonus();
+    }
+    if (isSpare()) {
+      score += spareBonus();
+    }
+    return score;
+  }
+  
+  private boolean isStrike() {
+    return firstBall() == 10 && (tenthFrame || secondBall() == 0);
+  }
+  
+  private boolean nextFrameIsStrike() {
+    return firstBallOfNextFrame() == 10;
+  }
+  
+  private boolean isSpare() {
+    return baseScore() == 10 && secondBall() > 0;
+  }
+  
+  private int baseScore() {
+    return firstBall() + secondBall();
+  }
+  
+  private int strikeBonus() {
+    int bonus = 0;
+    if (tenthFrame) {
+      bonus = bonusBall();
+    } else if (ninthFrame) {
+      bonus = firstBallOfNextFrame() + secondBallOfNextFrame();
+    } else if (nextFrameIsStrike()) {
+      bonus = firstBallOfNextFrame() + firstBallOfNextFramesNextFrame();
+    } else {
+      bonus = firstBallOfNextFrame() + secondBallOfNextFrame();
+    }
+    return bonus;
+  }
+  
+  private int spareBonus() {
+    return firstBallOfNextFrame();
+  }
+  
+  private int firstBall() {
+    return frameRolls[0];
+  }
+  
+  private int secondBall() {
+    return frameRolls[1];
+  }
+  
+  private int firstBallOfNextFrame() {
+    return frameRolls[2];
+  }
+  
+  private int secondBallOfNextFrame() {
+    return frameRolls[3];
+  }
+  
+  private int firstBallOfNextFramesNextFrame() {
+    return frameRolls[4];
+  }
+  
+  private int bonusBall() {
+    return firstBallOfNextFrame();
+  }
+  
 }
